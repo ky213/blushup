024ab725b5741c7a5bf913b5d59cfc7fd67597e6
yaaqoub.keddaoui <yaaqoub.keddaoui@eadn.dz>
2019-05-14 11:36:41 +0100

    Updates

---
+++ precache-manifest.346374268d9f77d138272f7a6b3317ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a17841d1a85d1b706c59a15d100577a",
+    "url": "/blushup/index.html"
+  },
+  {
+    "revision": "e35881956a19b9c65f04",
+    "url": "/blushup/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "360460bfc53889ae00e2",
+    "url": "/blushup/static/css/main.d56d6d6d.chunk.css"
+  },
+  {
+    "revision": "e35881956a19b9c65f04",
+    "url": "/blushup/static/js/2.a2efd4c4.chunk.js"
+  },
+  {
+    "revision": "360460bfc53889ae00e2",
+    "url": "/blushup/static/js/main.11bf42d0.chunk.js"
+  },
+  {
+    "revision": "3fce3bee15cb98eefeff",
+    "url": "/blushup/static/js/runtime~main.ee26e323.js"
+  },
+  {
+    "revision": "8c3d1d55aa76eeaab0c3d12509220c48",
+    "url": "/blushup/static/media/Hand.8c3d1d55.svg"
+  },
+  {
+    "revision": "3bfaf44a381cfd65ffe4a8675abcd395",
+    "url": "/blushup/static/media/ReviewStars.3bfaf44a.svg"
+  },
+  {
+    "revision": "75581d20d0ca8fcf0fb9fd6a3283529a",
+    "url": "/blushup/static/media/Shop.75581d20.png"
+  },
+  {
+    "revision": "8f5ce27564945d2c9a10ef827549a78c",
+    "url": "/blushup/static/media/facebook.8f5ce275.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/blushup/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blushup/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd3f05c965b765148c60dca5c6886682",
+    "url": "/blushup/static/media/logo.fd3f05c9.svg"
+  },
+  {
+    "revision": "0251d8ee95aa6d1f3400faa3b46b4bcf",
+    "url": "/blushup/static/media/twitter.0251d8ee.png"
+  }
+]);
